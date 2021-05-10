65c05144505a6942d45e0bc3bac44d54fda52f9f
Aakash Srivastava <aakashsrivastava013@gmail.com>
2021-05-10 10:45:29 +0530

    google oauth2.0 added

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ const path = require("path");
 const express = require("express");
 const { type } = require("os");
 const app = express();
+const passport = require("passport");
+require('./passport-setup'); 
+const cookieSession = require("cookie-session");
 
 app.set("views", "./pages");
 app.set("view engine", "ejs");
 app.use(express.static("./public"));
 app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieSession({
+  name: 'me-session',
+  keys: ['key1', 'key2']
+}));
+
+const isLoggedIn = (req,res,next) => {
+  if(req.user){
+    next();
+  }else{
+    res.sendStatus(401);
+  }
+}
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(
   "/css",
@@ -25,6 +44,25 @@ app.get("/", (req, res) => {
   res.render("home", { login: null });
 });
 
+app.get("/failed", (req, res) => {
+  res.render("login", { message: "Failed to login" });
+});
+
+app.get("/good",isLoggedIn, (req, res) => {
+  res.render("login", { message: "Welcome Mr. ${req.user.email}" });
+});
+
+app.get('/login',
+  passport.authenticate('google', { scope: ['profile','email'] }));
+
+app.get('/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/failed' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/home');
+  });
+
+
 app.get("/about", (req, res) => {
   res.render("about", { login: null });
 });
@@ -37,6 +75,12 @@ app.get("/products/:firm", (req, res) => {
 app.get("/orders", (req, res) => {
   res.render("products", { login: null, products: null });
 });
+
+app.get('/logout',(req,res) => {
+  req.session = null;
+  req.logout();
+  res.redirect("/");
+})
 
 app.listen(5000, (req, res) => {
   console.log("Server running on port 5000");
